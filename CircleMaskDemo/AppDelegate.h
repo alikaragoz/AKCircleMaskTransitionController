5925f6cbf784dd58731a89349f9a2ccc6ece5036
Ali Karagoz <mail@alikaragoz.net>
2014-12-19 14:31:19 +0100

    Initial Commit

---
+++ CircleMaskDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleMaskDemo
+//
+//  Created by Ali Karagoz on 19/12/14.
+//  Copyright (c) 2014 Ali Karagoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleMaskDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleMaskDemo
+//
+//  Created by Ali Karagoz on 19/12/14.
+//  Copyright (c) 2014 Ali Karagoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
